a25616e449cf5d28fe9e9dd442028363baa81c3d
weslleylancador <138718819+weslleylancador@users.noreply.github.com>
2024-04-30 18:41:55 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+
+      <GlobalStyle/>
+    </ThemeProvider>
+  )
+}
