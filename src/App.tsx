80539fccb39e9cb2772a1fbda43a80d08d8cfcdb
Weslley Cabral <weslley.deve@gmail.com>
2024-04-30 22:06:09 -0300

    Header & Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Button } from "./components/Button"
+
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { Router } from "./Router"
 
 export function App() {
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-
-      <GlobalStyle/>
-    </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <Router/>	
+        <GlobalStyle/>
+      </ThemeProvider>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { HeaderContainer } from "./style";
+
+export function Header() {
+    return(
+        <HeaderContainer>
+            <div>Logo</div>
+            <div>Menu</div>
+        </HeaderContainer>   
+    );
+}
