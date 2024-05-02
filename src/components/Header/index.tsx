1ae0c97f5b8506f5b38eac13b7a143c00207f0fe
weslleylancador <138718819+weslleylancador@users.noreply.github.com>
2024-05-02 13:17:19 -0300

    NavLink + Estilizações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
 import { HeaderContainer } from "./style";
+import LogoIgnite from "../../assets/logo.svg"
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
     return(
         <HeaderContainer>
-            <div>Logo</div>
-            <div>Menu</div>
+            <img src={LogoIgnite} alt="" />
+            <nav>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24}/>
+                </NavLink>
+                <NavLink to="/history" title="Histórico">
+                    <Scroll size={24}/>
+                </NavLink>
+            </nav>
         </HeaderContainer>   
     );
 }
